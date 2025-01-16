1d2fd1be053ba9c855744de32feea0304d7ad143
TranNhatNguyen2003 <nguyentrandcm@gmail.com>
2025-01-16 22:10:19 +0700

    CreateProject
    npm install.
    npm install express mongoose dotenv body-parser cors.
    npm install --save-dev nodemon

---
+++ BE/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected successfully!');
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  });
+
