0bd7fcb0dba4224adabab6b3ec7c2d49a74f8753
wasabiQuoc <wasabi.together@gmail.com>
2025-01-30 20:12:23 +0700

    BE_AV3 - CRUD create book

--- BE/src/routes/book.route.js
+++ BE/src/routes/book.route.js
@@ -17,6 +17,22 @@ router.post(
     ],
     BookController.addBook
 );
+
+router.post(
+    '/add-createbook',
+    [
+        body('bookname').notEmpty().withMessage('Bookname is required'),
+        body('author').notEmpty().withMessage('Author is required'),
+        body('categories').notEmpty().withMessage('Categories is required'),
+        body('price').notEmpty().withMessage('Price is required').isFloat({ min: 0 }).withMessage('Price must be a non-negative number'),
+        body('statusbook').notEmpty().withMessage('Statusbook is required'),
+        body('brief').notEmpty().withMessage('Brief is required'),
+        body('wordcontents').notEmpty().withMessage('Wordcontents is required'),
+        body('audios').notEmpty().withMessage('Audios is required'),
+    ],
+    BookController.addCreateBook
+);
+
 router.patch(
     '/update-book/:bid',
     [
@@ -31,6 +47,8 @@ router.patch(
     ],
     BookController.updateBook
 );
+
 router.delete('/delete-book/:bid', BookController.deleteBook);
+router.delete('/delete-createbook/:bid', BookController.deleteCreateBook);
 
 module.exports = router;
--- BE/src/services/book.service.js
+++ BE/src/services/book.service.js
@@ -1,4 +1,5 @@
 const Book = require('../models/book.model');
+const User = require('../models/user.model');
 const mongoose = require('mongoose');
 
 exports.addBook = async (bookdata) => {
@@ -67,6 +68,80 @@ exports.deleteBook = async (bookId) => {
         await sess.commitTransaction();
         sess.endSession();
         return 'Delete book successfull';
+
+    } catch (err) {
+        await sess.abortTransaction();
+        sess.endSession();
+        throw new Error('Something went wrong, could not delete book.', 404);
+    }
+};
+
+exports.addCreateBook = async (bookdata) => {
+    const { bookname, author, categories, price,
+        statusbook, brief, wordcontents, audios } = bookdata;
+
+
+    const existingBook = await Book.findOne({ bookname });
+    if (existingBook) {
+        throw new Error('Bookname already exists', 500);
+    }
+
+    const user = await User.findById(author);
+    if (!user) {
+        throw new Error('Bookname have not exists', 500);
+    }
+
+    const addedBook = new Book({
+        bookname,
+        author,
+        categories,
+        price,
+        statusbook,
+        brief,
+        wordcontents,
+        audios
+    });
+
+    try {
+        const sess = await mongoose.startSession();
+        sess.startTransaction();
+        await addedBook.save({ session: sess });
+        user.sachSangTac.push(addedBook);
+        await user.save({ session: sess });
+        await sess.commitTransaction();
+
+        return addedBook;
+    } catch (err) {
+        throw new Error('Creating book failed, please try again.', 500);
+    }
+};
+
+exports.deleteCreateBook = async (bookId) => {
+    let book, user;
+    try {
+        book = await Book.findById(bookId);
+        user = await User.findById(book.author);
+    } catch (err) {
+        throw new Error('Something went wrong, could not delete book.', 404);
+    }
+
+    if (!book) {
+        throw new Error('Could not find book for this id.', 404);
+    }
+
+    if (!user) {
+        throw new Error('Could not find user for this id.', 404);
+    }
+
+    const sess = await mongoose.startSession();
+    try {
+        sess.startTransaction();
+        await book.deleteOne({ session: sess });
+        user.sachSangTac.pull(bookId);
+        await user.save();
+        await sess.commitTransaction();
+        sess.endSession();
+        return 'Delete create book successfull';
 
     } catch (err) {
         await sess.abortTransaction();
